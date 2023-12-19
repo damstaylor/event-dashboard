f3e8bbf9846e803fa7f1bf5a0f6c39f7068538f4
Félicien François <felicien@alvarum.com>
2023-12-19 12:27:27 +0100

    fix eslint ecmaVersion

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-prettier/skip-formatting"],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 2020,
   },
   rules: {
     "no-unused-vars": ["warn", { ignoreRestSiblings: true, varsIgnorePattern: "Types|Models" }],
